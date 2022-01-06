732003b38812bf8dc1aff00d111f3796d545524d
Leonardo Lucio <leonardo_lucio_99@hotmail.com>
2022-01-06 14:56:47 -0300

    parteF

---
+++ Web/web/src/views/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as F from './style'
+
+import Header from '../../components/Header';
+
+function Home() {
+  return (
+  <F.Container>
+  <Header/>
+  </F.Container>
+    )
+}
+
+export default Home;
