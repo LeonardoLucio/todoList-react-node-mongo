acc1d3470313660469ae33ef9a8748edb197d53a
Leonardo Lucio <leonardo_lucio_99@hotmail.com>
2022-01-07 13:59:21 -0300

    front

--- Web/web/src/components/Header/styles.js
+++ Web/web/src/components/Header/styles.js
@@ -1,1 +1,21 @@
 import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    height: 70px;
+    background: red;
+    display: flex;
+    border-bottom: 5px solid #999;
+
+`
+export const LeftSide = styled.div`
+    width: 50%;
+    height: 70px;
+  
+`
+
+export const RightSide = styled.div`
+    width: 50%;
+    height: 70px;
+  
+`
