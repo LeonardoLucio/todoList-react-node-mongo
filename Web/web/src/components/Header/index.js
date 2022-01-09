f919963b3501f88451fc7746584290bbed576f83
LeonardoLucio <leonardo_lucio_99@hotmail.com>
2022-01-09 11:56:19 -0300

    header

--- Web/web/src/components/Header/index.js
+++ Web/web/src/components/Header/index.js
@@ -2,15 +2,22 @@ import React from 'react'
 import * as F from './styles'
 
 import logo from '../../assets/logo.png'
+import notification from '../../assets/notification.png'
 
 function Header() {
   return (
     <F.Container>
       <F.LeftSide>
-        
+        <img src={logo} alt="logo"></img>
       </F.LeftSide>
       <F.RightSide>
-        
+        <a href="#">INÍCIO</a>
+        <hr/>
+        <a href="#">NOVA TAREFA</a>
+        <hr/>
+        <a href="#">SINCRONIZAR CELULAR</a>
+        <hr/>
+        <img src={notification} alt="notification"></img>
       </F.RightSide>
     </F.Container>
   )
--- Web/web/src/components/Header/styles.js
+++ Web/web/src/components/Header/styles.js
@@ -5,17 +5,42 @@ export const Container = styled.div`
     height: 70px;
     background: red;
     display: flex;
-    border-bottom: 5px solid #999;
+    border-bottom: 5px solid #1d1d1d;
 
 `
 export const LeftSide = styled.div`
     width: 50%;
     height: 70px;
-  
+    
+    img {
+        width: 170px;
+        height: 55px;
+        padding-left: 5px;
+    }
+    display: flex;
+    align-items: center;
 `
 
 export const RightSide = styled.div`
     width: 50%;
     height: 70px;
-  
+    
+    a{
+        text-decoration: none;
+        color: white;
+    }
+    img {
+    width: 40px;   
+
+     }
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    
+    hr{
+        height: 80%; 
+        display: flex;
+
+    }
 `
